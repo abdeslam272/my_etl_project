f4050afc0a4e3edcd428900a3a8dae146328cb37
Abdeslam EL BENNOURI <71906221+abdeslam272@users.noreply.github.com>
2024-11-02 11:02:14 +0100

    Update dataset Files

--- dataset/export_to_csv.sh
+++ dataset/export_to_csv.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# Wait for PostgreSQL to be ready
-sleep 10
 
-# Export each table to CSV
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<EOF
-\copy actor TO '/app/exports/actor.csv' CSV HEADER;
-\copy film TO '/app/exports/film.csv' CSV HEADER;
-EOF
+# Wait for PostgreSQL to be ready
+until pg_isready -h localhost -p 5432 -U "$POSTGRES_USER"; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+done
+
+# Connect to the database and export tables to CSV
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+# Specify the tables to export
+tables=("film" "actor" "customer" "payment")  # Adjust table names as needed
+
+for table in "${tables[@]}"; do
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\COPY $table TO '/csv-output/${table}.csv' WITH CSV HEADER;"
+done
+
+echo "Data export to CSV completed."
